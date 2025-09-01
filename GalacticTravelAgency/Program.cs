e77c4b6612588a5146ce7943d822602c9e0b03f2
RegisteredLerner <pewdiejanusz@gmail.com>
2025-09-01 14:10:05 +0200

    added GalacticTravelAgency project

---
+++ GalacticTravelAgency/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string passengerName = "Zara";
+        int passengerAge = 28;
+        string ticketType = "First Class";
+        string prefferedPlanet = "Mars";
+        Console.WriteLine(passengerName);
+        Console.WriteLine(passengerAge);
+        Console.WriteLine(ticketType);
+        Console.WriteLine(prefferedPlanet);
+        passengerAge += 1;
+        Console.WriteLine(passengerAge);
+        double passengerAgeDouble = (double)passengerAge;
+        Console.WriteLine(passengerAgeDouble);
+        double passengerAgeDouble2 = passengerAge;
+        Console.WriteLine(passengerAgeDouble2);
+        string passengerAgeString = Convert.ToString(passengerAge);
+
+
+   }
+}
