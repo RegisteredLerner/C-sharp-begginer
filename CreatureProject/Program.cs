488efba04f0fc78c8f99f971b40d4fc7a9d9181c
RegisteredLerner <pewdiejanusz@gmail.com>
2025-09-01 13:12:38 +0200

    Creature Project

---
+++ CreatureProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {//Creating a monster creature #1 project C#
+        Console.WriteLine(" .-."); 
+        Console.WriteLine(" o o ");
+        Console.WriteLine("| 0 |");
+        Console.WriteLine("|   |");
+        Console.WriteLine("'~~~'");
+        Console.WriteLine("Gerson Creature");
+    }
+}
