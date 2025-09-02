8962d52b45ba8ef734140d3d7a4f11940e6e9ace
RegisteredLerner <pewdiejanusz@gmail.com>
2025-09-02 16:40:50 +0200

    Added space expedition project

---
+++ SpaceExpeditionBooleanAdventure/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//project about spaceexpedition
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isAtmosphereBreathable = true;
+        bool isGravityStable = false;
+        bool resourcesSufficient = true;
+        bool isHabitable = (isAtmosphereBreathable && isGravityStable);
+        Console.WriteLine(isHabitable);
+        bool suitableForExpansion = (isGravityStable || resourcesSufficient);
+        Console.WriteLine(suitableForExpansion);
+        int currentCrew = 5;
+        int maxCrewCapacity = 10;
+        bool canAcceptMoreCrew = (currentCrew < maxCrewCapacity);
+        Console.WriteLine(canAcceptMoreCrew);
+      
+   }
+}
